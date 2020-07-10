e48c71e226be8f243e41c1d6899c3e68cb40e43a
Maggie Mahan <maggiemahan@scien-dev1.c.flywheel-scien.internal>
2020-07-10 18:33:53 +0000

    MAINT: Python 3 upgrade with updates to standard gear practices.

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
-import json
+import flywheel
 from flywheel_bids.curate_bids import main_with_args
 
 if __name__ == '__main__':
 
-    # Grab Config
-    CONFIG_FILE_PATH = '/flywheel/v0/config.json'
-    with open(CONFIG_FILE_PATH) as config_file:
-        config = json.load(config_file)
+    with flywheel.GearContext() as gear_context:
 
-    api_key = config['inputs']['api_key']['key']
-    session_id = config['destination']['id']
-    reset = config['config']['reset']
-    subject_only = not config['config']['entire_project']
+        api_key = gear_context.get_input("api_key")['key']
+        session_id = gear_context.destination["id"]
+        reset = gear_context.config.get("reset")
+        subject_only = not gear_context.config.get("entire_project")
 
-    main_with_args(api_key, session_id, reset, subject_only)
+        main_with_args(api_key, session_id, reset, subject_only)
+
